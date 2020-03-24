8709bcf8e86b31e7761432846cb78630e7c05948
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-24 16:44:41 +0000

    Add conditional text + semanticui icon based on current month & lat

--- 03_seasons/src/SeasonDisplay.js
+++ 03_seasons/src/SeasonDisplay.js
@@ -10,8 +10,17 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
-    return <div>Season Display</div>;
+    const text = season === "winter" ? "Burr, it's chilly" : "Let's hit the beach";
+    const icon = season === "winter" ? "snowflake" : "sun";
+
+    return (
+        <div>
+            {/* className={ VALUE_OF_CONST_ICON CSS_SEMANTICUI_CLASS } */}
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
