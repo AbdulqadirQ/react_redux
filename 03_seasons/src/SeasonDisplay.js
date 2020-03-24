ed5e53d2047dcd92030515b87404b269dd520ce1
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-24 17:03:37 +0000

    css for changing background and semantic ui icons

--- 03_seasons/src/SeasonDisplay.js
+++ 03_seasons/src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 const seasonConfig = {
@@ -24,11 +25,11 @@ const SeasonDisplay = props => {
     const { text, iconName } = seasonConfig[season];
 
     return (
-        <div>
+        <div className={`season-display ${season}`}>
             {/* className={ VALUE_OF_seasonConfig.iconName CSS_SEMANTIC_UI_CLASS } */}
-            <i className={`${iconName} icon`} />
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 };
