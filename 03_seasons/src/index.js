0e1a67e76ce997dcd55a38818c86fe72b35952a8
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-24 16:11:09 +0000

    refactor constructor into short-hand

--- 03_seasons/src/index.js
+++ 03_seasons/src/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { lat: null, lon: null, errorMessage: "" };
-    }
+    state = { lat: null, lon: null, errorMessage: "" };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
