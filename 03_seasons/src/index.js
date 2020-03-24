1bc9c12b196fe5f1273237c0002a9d82721cf847
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-24 16:27:40 +0000

    determine winter or summer using function in SeasonDisplay

--- 03_seasons/src/index.js
+++ 03_seasons/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-    state = { lat: null, lon: null, errorMessage: "" };
+    state = { lat: null, errorMessage: "" };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
@@ -19,14 +20,8 @@ class App extends React.Component {
         if (this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-        if (!this.state.errorMessage && this.state.lat && this.state.lon) {
-            return (
-                <div>
-                    Latitude: {this.state.lat}
-                    <br />
-                    Longitude: {this.state.lon}
-                </div>
-            );
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
         } else {
             return <div>Loading ...</div>;
         }
