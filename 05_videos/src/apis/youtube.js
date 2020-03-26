9d8d642d785bc3d6aeba35aaaf002ef24a75f4cb
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-26 17:03:49 +0000

    call api with term from search bar

--- 05_videos/src/apis/youtube.js
+++ 05_videos/src/apis/youtube.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const KEY = "AIzaSyDVlU6CWxuPk1PDZXWQWnyyzQfoIsKgIbU";
 
+export const baseParams = {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+};
+
 export default axios.create({
-    baseURL: "https://www.googleapis.com/youtube/v3",
-    params: {
-        part: "snippet",
-        type: "video",
-        maxResults: 5,
-        key: KEY
-    }
+    baseURL: "https://www.googleapis.com/youtube/v3"
 });
