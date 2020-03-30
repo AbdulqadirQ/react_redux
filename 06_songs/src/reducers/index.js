68d91875b0ebcc21be0315fd66627a82c2036280
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-30 14:26:42 +0100

    REDUCER NOTES from my understanding because im very confusion

--- 06_songs/src/reducers/index.js
+++ 06_songs/src/reducers/index.js
@@ -1,5 +1,21 @@
 import { combineReducers } from "redux";
 
+/*
+NOTES:
+These are setup as reducers and therefore make up the entire state of the App (in this case - if there were other reducers
+    then they would also contribute)
+SongReducer is just a fixed 'state' object and therefore cannot be changed - in this case is therefore the database
+SelectedSongReducer will actually take an 'action' as an argument which would change its state
+
+Both are exported as Reducers using 'combineReducers'. They're also renamed to 'songs' and 'selectedSong' respectively, which
+is what will appear in console if state is printed out
+
+SongList uses 'connect' which somehow allows access to store:
+    - uses songsReducer when mapStateToProps returns 'state.songs'
+    - uses  SelectedSongReducer when 'this.props.selectSong' refers to the Action Creator 'selectSong'
+      inside /src/actions/index.js
+*/
+
 const songsReducer = () => {
     return [
         { title: "No Scrubs", duration: "4:05" },
