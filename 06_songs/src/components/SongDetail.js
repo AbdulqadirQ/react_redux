85c6be9b3609c034b041dfaee1a3ca70a5da72a5
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-30 16:10:02 +0100

    Render selected song if exists

--- 06_songs/src/components/SongDetail.js
+++ 06_songs/src/components/SongDetail.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = props => {
-    console.log(props);
-    return <div>Song Detail</div>;
+const SongDetail = ({ currentSong }) => {
+    if (!currentSong) {
+        return <div>Select a song</div>;
+    }
+    return <div>{currentSong.title}</div>;
 };
 
 const mapStateToProps = state => {
