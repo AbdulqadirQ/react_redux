0cb4e5405691a51c3de23ff7be3fac5361b9dd78
Abdulqadir Qasim <tiamat@andromeda.local>
2020-03-31 12:53:11 +0100

    Wire-in Redux thunk for async data loading through Redux

--- 07_blog/src/actions/index.js
+++ 07_blog/src/actions/index.js
@@ -1,10 +1,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async dispatch => {
+    // Thunk recursively iterates through response until it's not a function
     const response = await jsonPlaceholder.get("/posts");
 
-    return {
+    // Action is dispatched manually to 'dispatch' in the event we have the raw Action and not a function which could be
+    // invoked to return an Action
+    dispatch({
         type: "FETCH_POSTS",
         payload: response
-    };
+    });
 };
--- 07_blog/src/index.js
+++ 07_blog/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import think from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
+import thunk from "redux-thunk";
+
+// links middleware 'thunk' to redux store
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector("#root")
