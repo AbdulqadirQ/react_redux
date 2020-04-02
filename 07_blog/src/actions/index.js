0b2770c3b963ae04e92e8f14dd9914f648face61
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-02 15:09:20 +0100

    Alternative and less complex solution for no redundant user requests

--- 07_blog/src/actions/index.js
+++ 07_blog/src/actions/index.js
@@ -1,6 +1,19 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    // await ensures fetchPosts is called, sends api request, gets response, and the dispatches action before moving on to next line of code
+    // So await IS ONLY used here since we have more logic following this line
+    await dispatch(fetchPosts());
+
+    // lodash 'map' gets only 'userId'  properties from getState().posts objects
+    // lodash 'uniq' returns an array of only unique userIds
+    const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+    // dispatches the response of fetchUser to redux store after we've made the request for specific userId
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
     // Thunk recursively iterates through response until it's not a function
     const response = await jsonPlaceholder.get("/posts");
@@ -13,10 +26,17 @@ export const fetchPosts = () => async dispatch => {
     });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+//  MEMOIZE ALTERNATIVE OF FETCHING ONLY UNIQUE USERS
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+// });
