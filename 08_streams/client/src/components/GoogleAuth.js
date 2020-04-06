4cce8b0d52f5e9d979d72c425cd0bc5fb3a2913f
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-06 15:53:20 +0100

    Implement messages based on signed-in state

--- 08_streams/client/src/components/GoogleAuth.js
+++ 08_streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,39 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
     componentDidMount() {
         // loads google auth2 libraries
         // use of a callback function to input clientId
         window.gapi.load("client:auth2", () => {
-            window.gapi.client.init({
-                clientId: "1015240795940-b5iu95u2o17bqpu86g7min89pfribv7d.apps.googleusercontent.com",
-                scope: "email"
-            });
+            // 'init()' returns a promise when a library has successfully initialised
+            window.gapi.client
+                .init({
+                    clientId: "1015240795940-b5iu95u2o17bqpu86g7min89pfribv7d.apps.googleusercontent.com",
+                    scope: "email"
+                })
+                .then(() => {
+                    // binds 'auth' so this.auth now returns an AuthInstance whenever called within the current class
+                    this.auth = window.gapi.auth2.getAuthInstance();
+                    // sets some component-level state
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                });
         });
     }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>I don't know if we're signed in</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in!</div>;
+        } else {
+            return <div>I am not signed in</div>;
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
