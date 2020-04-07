242be64242570564c06d6b82b1156aaf7e129380
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-07 13:34:49 +0100

    Send requests to json server using Axios with Redux Thunk and new ActionCreator

--- 08_streams/client/src/components/streams/StreamCreate.js
+++ 08_streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
     renderError({ error, touched }) {
@@ -26,9 +28,9 @@ class StreamCreate extends React.Component {
         );
     };
 
-    onSubmit(formValues) {
-        console.log(formValues);
-    }
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+    };
 
     render() {
         return (
@@ -62,7 +64,9 @@ const validate = (formValues) => {
 };
 
 // reduxForm takes the place of 'connect' and 'mapStateToProps' to handle Redux things, and passes props to above class
-export default reduxForm({
+const formWrapped = reduxForm({
     form: "streamCreate",
     validate: validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
