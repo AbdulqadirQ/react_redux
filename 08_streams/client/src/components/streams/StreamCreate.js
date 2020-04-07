e4eb501ab40ba05d9fdda92ae55ef2c3e5066ab6
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-07 10:52:05 +0100

    Add label as new prop on Field

--- 08_streams/client/src/components/streams/StreamCreate.js
+++ 08_streams/client/src/components/streams/StreamCreate.js
@@ -3,17 +3,22 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
     // `Field` tag below passes in some arguments in renderInput to allow forms to work
-    renderInput({ input }) {
+    renderInput({ input, label }) {
         // Adds all values within 'formProps.input' and add them to <input> tag
-        return <input {...input} />;
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
     }
 
     render() {
         return (
             // Field allows normal html forms to be wired-up to redux-form
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
             </form>
         );
     }
