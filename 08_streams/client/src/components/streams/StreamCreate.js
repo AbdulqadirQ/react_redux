ac9e128e984ccecd11374c0bd1d81b045ed3296c
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-07 10:21:07 +0100

    ----- Redux Form ---- basic form setup

--- 08_streams/client/src/components/streams/StreamCreate.js
+++ 08_streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+    // `Field` tag below passes in some arguments in renderInput to allow forms to work
+    renderInput(formProps) {
+        return <input onChange={formProps.input.onChange} value={formProps.input.value} />;
+    }
 
-export default StreamCreate;
+    render() {
+        return (
+            // Field allows normal html forms to be wired-up to redux-form
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    }
+}
+
+// reduxForm takes the place of 'connect' and 'mapStateToProps' to handle Redux things, and passes props to above class
+export default reduxForm({
+    form: "streamCreate",
+})(StreamCreate);
