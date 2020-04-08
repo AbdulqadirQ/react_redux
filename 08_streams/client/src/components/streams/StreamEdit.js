7bec7d77a86c2309ce27fcb19408192217147d8c
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-08 11:31:06 +0100

    Return stream data which matches selected stream

--- 08_streams/client/src/components/streams/StreamEdit.js
+++ 08_streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
+    console.log(props);
     return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+// The ID of the stream to edit is passed as a prop to StreamEdit. To get this using Redux, 'ownProps' holds
+// all props that would appear in StreamEdit
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps)(StreamEdit);
