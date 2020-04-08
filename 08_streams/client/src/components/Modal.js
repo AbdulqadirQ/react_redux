9a4c738c6e9f560e6f94987d2a3648b6e3e14fa1
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-08 16:45:41 +0100

    Further abstract Modal - pass onDismiss path to push

--- 08_streams/client/src/components/Modal.js
+++ 08_streams/client/src/components/Modal.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
     return ReactDOM.createPortal(
-        <div onClick={() => history.push("/")} className="ui dimmer modals visible active">
+        <div onClick={props.onDismiss} className="ui dimmer modals visible active">
             {/* stopPropagation ensures any clicks within the modal doesn't propagate above, so clicking inside doesn't close modal */}
             <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header">{props.title}</div>
--- 08_streams/client/src/components/streams/StreamDelete.js
+++ 08_streams/client/src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
     const actions = (
@@ -11,7 +12,12 @@ const StreamDelete = () => {
     return (
         <div>
             StreamDelete
-            <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions} />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+                onDismiss={() => history.push("/")}
+            />
         </div>
     );
 };
